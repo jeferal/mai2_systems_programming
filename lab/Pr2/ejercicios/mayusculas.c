e93b475d0cc2eb446e910a916bc7b49beef1b3e4
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2020-10-29 22:51:18 +0100

    Modified isalpha for isupper

--- lab/Pr2/ejercicios/mayusculas.c
+++ lab/Pr2/ejercicios/mayusculas.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <ctype.h>
 #define TAM_CADENA 200
 
-void main(){
+int main(){
 
     //Puntero a caracter para copiar las cadenas
     char *p1, *p2;
@@ -13,16 +14,22 @@ void main(){
     p1 = cadena;
     p2 = cadena2;
         
+    int i = 0;
     while(*p1 != '\0'){
         //Copiar p1 a p2 restando 32
-        if(isalpha(*p1))    *p2 = *p1;
-        
-        else    *p2 = *p1-32;
-        
+        if(isupper(*p1)){
+            *p2 = *p1;
+        }
+        else{
+             *p2 = *p1-32;
+        }    
         *p1++;
         *p2++;
+        i++;
     }
     
     *p2 = '\0';
     printf("%s", cadena2);
+
+    return 0;
 }
