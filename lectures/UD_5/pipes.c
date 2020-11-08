5b22c9a9072ee29d3801b517a37435f344f998c5
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2020-11-08 23:48:48 +0100

    Added new pipes example

---
+++ lectures/UD_5/pipes.c
@@ -0,0 +1,35 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+    int fd[2];
+    int val_fork;
+
+    pipe(fd);
+
+    if( val_fork=fork() == 0){
+        //Código que hace el hijo
+        //Pipe de salida
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+
+        //La salida de ls -l va al pipe
+        execl("/bin/ls", "ls", "-l", NULL);
+        printf("Ha fallado la ejecucion del ls");
+
+    } else {
+        //Código del padre
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execl("/usr/bin/sort", "sort", "-n", NULL);
+        printf("Ha fallado la ejecución del sort");
+    }
+
+    exit(0);
+    
+}
