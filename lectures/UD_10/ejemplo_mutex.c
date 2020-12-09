3f401c344e794c654e4f524aaac1f1557efb2b46
Jesús Ferrándiz <jeferrandiz98@gmail.com>
2020-12-09 22:35:02 +0100

    clearer

--- lectures/UD_10/ejemplo_mutex.c
+++ lectures/UD_10/ejemplo_mutex.c
@@ -31,7 +31,6 @@ int main ( ) {
 
     pthread_t hilo1, hilo2;
     pthread_attr_t atributos;
-
     pthread_attr_init(&atributos);
 
     pthread_create(&hilo1, &atributos, fhilo1, NULL);
@@ -39,6 +38,6 @@ int main ( ) {
 
     pthread_join(hilo1,NULL);
     pthread_join(hilo2,NULL);
-    
+
     printf("Value of V: %d\n", V);
 } 
